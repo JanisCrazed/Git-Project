9247965b06aec846b9994b26c8e3bdcc7d4f36d6
JanisCrazed <salili.jb.bscs@gmail.com>
2026-09-24 12:55:09 +0800

    add changes to Git-Project

--- C++/info.cpp
+++ C++/info.cpp
@@ -24,63 +24,72 @@ int checkId(string studentId){
 }
 
 void addData(string studentId){
-    int Id = checkId(studentId);
-    cout<<"Student ID : "<<studentId<<endl;
-    cout<<"Which data wouldyou like to add?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"-->";
-    int act;
-    cin>>act;
-    if(act == 1 && info[Id][0][1] == ""){
-        cout<<"Enter a Student ID"<<endl<<"Student ID : ";
-        getline(cin >> ws, info[Id][0][1]);
-    } else if(act == 2 && info[Id][1][1] == ""){
-        cout<<"Enter a new Student Name"<<endl<<"Name : ";
-        getline(cin >> ws, info[Id][1][1]);
-    } else if (act == 3 && info[Id][2][1] == ""){
-        cout<<"Enter a Student Age"<<endl<<"Age : ";
-        getline(cin >> ws, info[Id][2][1]);
-    } else if (act == 4 && info[Id][3][1] == ""){
-        cout<<"Enter aStudent Gender"<<endl<<"Gender : ";
-        getline(cin >> ws, info[Id][3][1]);
-    } else if (act == 5 && info[Id][4][1] == ""){
-        cout<<"Enter a Student Contact"<<endl<<"Contact : ";
-        getline(cin >> ws, info[Id][4][1]);
-    } else if (act == 6 && info[Id][5][1] == ""){
-        cout<<"Enter a Student Address"<<endl<<"Address : ";
-        getline(cin >> ws, info[Id][5][1]);
-    } else if(act <= 6 && act >= 0){
-        cout<<"This data is already occupied, would you like to edit it?(1: Yes)";
-        int Eact;
-        cin>>Eact;
-        if(Eact == 1){
-            editData(studentId);
+    while(true){
+        system("cls");
+        int Id = checkId(studentId);
+        cout<<endl<<"Student ID : "<<studentId<<endl;
+        cout<<"Which data wouldyou like to add?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"7:Go Back"<<endl<<"-->";
+        int act;
+        cin>>act;
+        system("cls");
+        if(act == 1 && info[Id][0][1] == ""){
+            cout<<"Enter a Student ID"<<endl<<"Student ID : ";
+            getline(cin >> ws, info[Id][0][1]);
+        } else if(act == 2 && info[Id][1][1] == ""){
+            cout<<"Enter a new Student Name"<<endl<<"Name : ";
+            getline(cin >> ws, info[Id][1][1]);
+        } else if (act == 3 && info[Id][2][1] == ""){
+            cout<<"Enter a Student Age"<<endl<<"Age : ";
+            getline(cin >> ws, info[Id][2][1]);
+        } else if (act == 4 && info[Id][3][1] == ""){
+            cout<<"Enter aStudent Gender"<<endl<<"Gender : ";
+            getline(cin >> ws, info[Id][3][1]);
+        } else if (act == 5 && info[Id][4][1] == ""){
+            cout<<"Enter a Student Contact"<<endl<<"Contact : ";
+            getline(cin >> ws, info[Id][4][1]);
+        } else if (act == 6 && info[Id][5][1] == ""){
+            cout<<"Enter a Student Address"<<endl<<"Address : ";
+            getline(cin >> ws, info[Id][5][1]);
+        } else if (act == 7){ 
+            break;
+        } else if(act <= 6 && act >= 0){
+            cout<<"This data is already occupied, would you like to edit it?(1: Yes)"<<endl<<"-->";
+            int Eact = 0;
+            cin>>Eact;
+            if(Eact == 1){
+                system("cls");
+                editData(studentId);
+            } else{
+                system("cls");
+                addData(studentId);
+            }
         } else{
+            cout<<"Invalid type, please input the exact number";
             addData(studentId);
         }
-    } else{
-        cout<<"Invalid type, please input the exact number";
-        addData(studentId);
     }
 }
 
 void searchData(string studentId){
-    int Id = checkId(studentId);
     int success;
     string search;
     while(true){
         success = 0;
-        cout<<"Student ID : "<<studentId<<endl;
+        cout<<endl<<"Searching all student data"<<endl;
         cout<<"     ---Search a Data---"<<endl<<"Type the exact value or type 'stop' to stop."<<endl;
         cout<<"-->";
         getline(cin >> ws, search);
         if(search == "stop"){
             system("cls");
             break;
-        }else{    
-            for(int i = 0; i <= 6; i++){
-                if(info[Id][i][1] == search){
-                    system("cls");
-                    cout<<search<<" is a "<<info[Id][i][0]<<" Data Value."<<endl;
-                    success = 1;
+        }else{   
+            for(int k = 0; k <=2; k++){
+                for(int i = 0; i <= 6; i++){
+                    if(info[k][i][1] == search){
+                        system("cls");
+                        cout<<search<<" is a "<<info[k][i][0]<<" Data Value of Student ID: "<<info[k][0][1]<<endl;
+                        success = 1;
+                    }
                 }
             }
             if (success != 1){
@@ -93,135 +102,156 @@ void searchData(string studentId){
 }
 
 void editData(string studentId){
-    int Id = checkId(studentId);
-    cout<<"Student ID : "<<studentId<<endl;
-    cout<<"Which data wouldyou like to edit?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"-->";
-    int act;
-    cin>>act;
-    if(act == 1 && info[Id][0][1] != ""){
-        cout<<"Enter a Student ID"<<endl<<"Student ID : ";
-        getline(cin >> ws, info[Id][0][1]);
-    } else if(act == 2 && info[Id][1][1] != ""){
-        cout<<"Enter a new Student Name"<<endl<<"Name : ";
-        getline(cin >> ws, info[Id][1][1]);
-    } else if (act == 3 && info[Id][2][1] != ""){
-        cout<<"Enter a Student Age"<<endl<<"Age : ";
-        getline(cin >> ws, info[Id][2][1]);
-    } else if (act == 4 && info[Id][3][1] != ""){
-        cout<<"Enter aStudent Gender"<<endl<<"Gender : ";
-        getline(cin >> ws, info[Id][3][1]);
-    } else if (act == 5 && info[Id][4][1] != ""){
-        cout<<"Enter a Student Contact"<<endl<<"Contact : ";
-        getline(cin >> ws, info[Id][4][1]);
-    } else if (act == 6 && info[Id][5][1] != ""){
-        cout<<"Enter a Student Address"<<endl<<"Address : ";
-        getline(cin >> ws, info[Id][5][1]);
-    } else if(act >= 0 && act <= 6){
-        for(int i = 0; i <= 5; i++){
-            if(i == act && info[Id][i][1] == ""){
-                cout<<"The data is Empty, would you like to add data?(1: Yes)";
-                int Eact;
-                cin>>Eact;
-                if(Eact == 1){
-                    addData(studentId);
-                }else{
-                    editData(studentId);
+    while(true){
+        system("cls");
+        int Id = checkId(studentId);
+        cout<<endl<<"Student ID : "<<studentId<<endl;
+        cout<<"Which data wouldyou like to edit?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"7:Go Back"<<endl<<"-->";
+        int act;
+        cin>>act;
+        if(act == 1 && info[Id][0][1] != ""){
+            cout<<"Enter a Student ID"<<endl<<"Student ID : ";
+            getline(cin >> ws, info[Id][0][1]);
+        } else if(act == 2 && info[Id][1][1] != ""){
+            cout<<"Enter a new Student Name"<<endl<<"Name : ";
+            getline(cin >> ws, info[Id][1][1]);
+        } else if (act == 3 && info[Id][2][1] != ""){
+            cout<<"Enter a Student Age"<<endl<<"Age : ";
+            getline(cin >> ws, info[Id][2][1]);
+        } else if (act == 4 && info[Id][3][1] != ""){
+            cout<<"Enter aStudent Gender"<<endl<<"Gender : ";
+            getline(cin >> ws, info[Id][3][1]);
+        } else if (act == 5 && info[Id][4][1] != ""){
+            cout<<"Enter a Student Contact"<<endl<<"Contact : ";
+            getline(cin >> ws, info[Id][4][1]);
+        } else if (act == 6 && info[Id][5][1] != ""){
+            cout<<"Enter a Student Address"<<endl<<"Address : ";
+            getline(cin >> ws, info[Id][5][1]);
+        } else if (act == 7){
+            break;
+        } else if(act >= 0 && act <= 6){
+            for(int i = 0; i <= 5; i++){
+                if(i == act && info[Id][i][1] == ""){
+                    cout<<"The data is Empty, would you like to add data?(1: Yes)"<<endl<<"-->";
+                    int Eact = 0;
+                    cin>>Eact;
+                    if(Eact == 1){
+                        system("cls");
+                        addData(studentId);
+                    }else{
+                        system("cls");
+                        editData(studentId);
+                    }
                 }
             }
+        } else{
+            cout<<"Invalid type, please input the exact number";
+            editData(studentId);
         }
-    } else{
-        cout<<"Invalid type, please input the exact number";
-        editData(studentId);
     }
 }   
 
 void deleteData(string studentId){
-    int Id = checkId(studentId);
-    cout<<"Student ID : "<<studentId<<endl;
-    cout<<"Which data wouldyou like to Delete?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"-->";
-    int act;
-    int sure;
-    cin>>act;
-    if(act == 1 && info[Id][0][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Student ID : "<<info[Id][0][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][0][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if(act == 2 && info[Id][1][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Name : "<<info[Id][1][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][1][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if (act == 3 && info[Id][2][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Age : "<<info[Id][2][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][2][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if (act == 4 && info[Id][3][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Gender : "<<info[Id][3][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][3][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if (act == 5 && info[Id][4][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Contact : "<<info[Id][4][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][4][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if (act == 6 && info[Id][5][1] != ""){
-        cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Address : "<<info[Id][5][1]<<endl<<"-->";
-        cin>>sure;
-        if(sure == 1){
-            info[Id][5][1] = "";
-        }else{
-            system("cls");
-            cout<<"Type 1 if you want to delete"<<endl;
-            deleteData(studentId);
-        }
-    } else if(act >= 0 && act <= 6){
-        for(int i = 0; i <= 5; i++){
-            if(i == act && info[Id][i][1] == ""){
-                cout<<"The data is Empty, would you like to add data?(1: Yes)";
-                int Eact;
-                cin>>Eact;
-                if(Eact == 1){
-                    addData(studentId);
-                }else{
-                    deleteData(studentId);
+    while(true){
+        system("cls");
+        int Id = checkId(studentId);
+        cout<<endl<<"Student ID : "<<studentId<<endl;
+        cout<<"Which data wouldyou like to Delete?(type a number only!)"<<endl<<"1:Student ID"<<endl<<"2:Name"<<endl<<"3:Age"<<endl<<"4:Gender"<<endl<<"5:Contact"<<endl<<"6:Address"<<endl<<"7:Go Back"<<endl<<"-->";
+        int act = 0;
+        int sure = 0;
+        cin>>act;
+        if(act == 1 && info[Id][0][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Student ID : "<<info[Id][0][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][0][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if(act == 2 && info[Id][1][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Name : "<<info[Id][1][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][1][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if (act == 3 && info[Id][2][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Age : "<<info[Id][2][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][2][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if (act == 4 && info[Id][3][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Gender : "<<info[Id][3][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][3][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if (act == 5 && info[Id][4][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Contact : "<<info[Id][4][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][4][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if (act == 6 && info[Id][5][1] != ""){
+            cout<<"Are you sure to delete?(1:Yes)"<<endl<<"Address : "<<info[Id][5][1]<<endl<<"-->";
+            cin>>sure;
+            if(sure == 1){
+                info[Id][5][1] = "";
+                system("cls");
+            }else{
+                system("cls");
+                cout<<"Type 1 if you want to delete"<<endl;
+                deleteData(studentId);
+            }
+        } else if (act == 7){
+            break;
+        } else if(act >= 0 && act <= 6){
+            for(int i = 0; i <= 5; i++){
+                if(i == act && info[Id][i][1] == ""){
+                    cout<<"The data is Empty, would you like to add data?(1: Yes)";
+                    int Eact;
+                    cin>>Eact;
+                    if(Eact == 1){
+                        system("cls");
+                        addData(studentId);
+                    }else{
+                        system("cls");
+                        deleteData(studentId);
+                    }
                 }
             }
+        } else{
+            cout<<"Invalid type, please input the exact number";
+            deleteData(studentId);
         }
-    } else{
-        cout<<"Invalid type, please input the exact number";
-        deleteData(studentId);
     }
 }
 
 void viewAllData(string studentId){
+    
     int Id = checkId(studentId);
     cout<<"     ---Viewing Data---"<<endl<<endl;
     if(Id == 99){
@@ -248,11 +278,11 @@ void viewAllData(string studentId){
             cout<<endl;
         }
     }
-    cout<<endl<<endl;   
+       
 }
 
 void method(string studentId){
-    int perform;
+    int perform = 0;
     cout<<"Student ID : "<<studentId<<endl;
     cout<<"Which would you like to perform: "<<endl<<"1:Add a Data"<<endl<<"2:Edit a Data"<<endl<<"3:Delete a Data"<<endl<<"4:Search a Data"<<endl<<"5:View All Data"<<endl<<"6:Go Back"<<endl<<"-->";
     cin>>perform;
@@ -275,7 +305,16 @@ void method(string studentId){
     }else if(perform == 5){
         resume = 0;
         system("cls");
-        viewAllData(studentId); 
+        int view = 0;
+        cout<<"Type (1:View Student Only) (2:View All Student)"<<endl<<"-->";
+        cin>>view;
+        if(view == 1){
+            system("cls");
+            viewAllData(studentId); 
+        } else if(view >= 2){
+            system("cls");
+            viewAllData("All");
+        }
     }else if(perform == 6){
         system("cls");
         resume = 1; 
@@ -290,7 +329,7 @@ int main(){
     cout<<"----------Welcome----------"<<endl;
     cout<<endl<<"   -----Current Data-----"<<endl;
     viewAllData("All");
-    int Id;
+    int Id = 0;
     string studentId;
     while(true){
         cout<<"Type the EXACT Student Id to interact(or type 'stop' to end the session)"<<endl<<"-->";   
@@ -311,6 +350,9 @@ int main(){
                         method(studentId);
                     }else{
                         resume = 0;
+                        for(int i = 0; i <= 2; i++){
+                            cout<<info[i][0][0]<<" : "<<info[i][0][1]<<endl;                      
+                        }
                         break;
                     }
                 }
